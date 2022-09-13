6ef62d36aa06853d3060293e3ae3da1384315b0f
LLUC MATAS <llucmatas@inspladelestany.cat>
2022-09-13 19:11:13 +0200

    UnitatFormativa

---
+++ main.py
@@ -0,0 +1,17 @@
+# Definició de classes
+# Lluc i Gerard
+class UnitatFormativa:
+    nom = None
+
+    def __init__(self, nom):
+        self.nom = nom
+
+
+# Inici del programa
+uf1 = UnitatFormativa("UF1. Desenvolupament del programari")
+uf2 = UnitatFormativa("UF2. Optimització del programari")
+uf3 = UnitatFormativa("UF3. Introducció al Disseny Orientat a Objectes")
+
+print(uf1.nom)
+print(uf2.nom)
+print(uf3.nom)
