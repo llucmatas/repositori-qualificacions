c5407f17a9329c66aaa7a106bc8201d4fb00325c
Gerard Trabazos Soler <gerardtrabazosinspladelestany.cat>
2022-09-16 17:20:42 +0200

    afegim atribut 'qualificacio' a la classe Unitat formativa

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # Lluc i Gerard
 class UnitatFormativa:
     nom = None
+    qualificacions = None
 
     def __init__(self, nom):
         self.nom = nom
@@ -12,6 +13,10 @@ uf1 = UnitatFormativa("UF1. Desenvolupament del programari")
 uf2 = UnitatFormativa("UF2. Optimització del programari")
 uf3 = UnitatFormativa("UF3. Introducció al Disseny Orientat a Objectes")
 
-print(uf1.nom)
-print(uf2.nom)
-print(uf3.nom)
+uf1.qualificacions = 8
+uf2.qualificacions = 10
+uf3.qualificacions = 4
+
+print(uf1.nom, ":", uf1.qualificacions)
+print(uf2.nom, ":", uf1.qualificacions)
+print(uf3.nom, ":", uf1.qualificacions)
