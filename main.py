ce9c9ea136d174188ce62f4596791ab0cf27b73a
LLUC MATAS <llucmatas@inspladelestany.cat>
2022-09-20 18:33:13 +0200

    afegim més codi

--- main.py
+++ main.py
@@ -1,22 +1,53 @@
 # Definició de classes
 # Lluc i Gerard
+# Definició de classes
 class UnitatFormativa:
     nom = None
-    qualificacions = None
+    qualificacio = None
+    hores = None
+
+    def __init__(self, nom, hores):
+        self.nom = nom
+        self.hores = hores
+
+
+class ModulProfessiona:
+    nom = None
+    unitats_formatives = []
 
     def __init__(self, nom):
         self.nom = nom
 
+    def afegirUnitatFormativa(self, unitat_formativa):
+        self.unitats_formatives.append(unitat_formativa);
+
+    def getQualificacio(self):
+        suma_hores = 0
+        suma_qualificacio = 0
+
+        for uf in self.unitats_formatives:
+            suma_hores += uf.hores
+            suma_qualificacio += (uf.qualificacio * uf.hores)
+
+        return suma_qualificacio / suma_hores
+
 
 # Inici del programa
-uf1 = UnitatFormativa("UF1. Desenvolupament del programari")
-uf2 = UnitatFormativa("UF2. Optimització del programari")
-uf3 = UnitatFormativa("UF3. Introducció al Disseny Orientat a Objectes")
+uf1 = UnitatFormativa("UF1. Desenvolupament del programari", 20)
+uf2 = UnitatFormativa("UF2. Optimització del programari", 20)
+uf3 = UnitatFormativa("UF3. Introducció al Disseny Orientat a Objectes", 26)
 
-uf1.qualificacions = 8
-uf2.qualificacions = 10
-uf3.qualificacions = 4
+uf1.qualificacio = 8
+uf2.qualificacio = 10
+uf3.qualificacio = 4
 
-print(uf1.nom, ":", uf1.qualificacions)
-print(uf2.nom, ":", uf1.qualificacions)
-print(uf3.nom, ":", uf1.qualificacions)
+mp5 = ModulProfessiona("MP05. Entorns de desenvolupament")
+
+mp5.afegirUnitatFormativa(uf1)
+mp5.afegirUnitatFormativa(uf2)
+mp5.afegirUnitatFormativa(uf3)
+
+print(uf1.nom, ":", uf1.qualificacio)
+print(uf2.nom, ":", uf2.qualificacio)
+print(uf3.nom, ":", uf3.qualificacio)
+print(mp5.nom, ":", mp5.getQualificacio())
